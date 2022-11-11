b5f226e3309a504dd253bdb273b98fe911e5d3d0
Leslie Suh <lesliejysuh@gmail.com>
2022-11-11 17:28:09 +0000

    yay

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 function App() {
   const videoRef = useRef(null);
   const photoRef = useRef(null);
+
+  const [hasPhoto, setHasPhoto] = useState(false);
 
   const getVideo = () => {
     navigator.mediaDevices
@@ -26,6 +28,10 @@ function App() {
       <div className="camera">
         <video ref={videoRef}></video>
         <button>SNAP!</button>
+      </div>
+      <div className={"result" + (hasPhoto ? " hasPhoto" : "")}>
+        <canvas ref={photoRef}></canvas>
+        <button>CLOSE!</button>
       </div>
     </div>
   );
