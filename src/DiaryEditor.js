5ad902c3d3b62f405b2a81939160b634387b565b
Leslie Suh <lesliejysuh@gmail.com>
2022-11-12 13:47:20 +0000

    added emotion options

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,32 +5,52 @@ const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    alert("Saved!");
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>Today's Diary</h2>
       <div>
         <input
+          name="author"
           value={state.author}
-          onChange={(e) => {
-            setState({
-              author: e.target.value,
-              content: state.content,
-            });
-          }}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
+          name="content"
           value={state.content}
-          onChange={(e) => {
-            setState({
-              content: e.target.value,
-              author: state.author,
-            });
-          }}
+          onChange={handleChangeState}
         />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>Save</button>
       </div>
     </div>
   );
